46f8b902a970b7c251b81160a19c509cc61c7b7d
Xwilarg <xwilarg@protonmail.com>
2022-04-22 21:18:42 +0200

    Add AI movement between nodes

---
+++ Assets/Scripts/Player/AI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace MiniJameGam9.Player
+{
+    public class AI : MonoBehaviour
+    {
+        private Transform _currentNode;
+        private NavMeshAgent _agent;
+
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Start()
+        {
+            GetNextNode();
+        }
+
+        private void Update()
+        {
+            if (Vector2.Distance(FlattenY(transform.position), FlattenY(_currentNode.position)) < .1f)
+            {
+                GetNextNode();
+            }
+        }
+
+        private Vector2 FlattenY(Vector3 v)
+            => new Vector2(v.x, v.z);
+
+        private void GetNextNode()
+        {
+            _currentNode = AIManager.Instance.NextNode(_currentNode);
+            _agent.SetDestination(_currentNode.position);
+        }
+    }
+}
