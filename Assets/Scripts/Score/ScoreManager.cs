75af3e1ab6e41bbacd2984fe38701a41b55e0a22
Xwilarg <xwilarg@protonmail.com>
2022-04-24 11:21:53 +0200

    Begin to implement score system

---
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using MiniJameGam9.Character;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniJameGam9.Score
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        private static ScoreManager _instance;
+        public static ScoreManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var go = new GameObject("Score Manager", typeof(ScoreManager));
+                    _instance = go.AddComponent<ScoreManager>();
+                }
+                return _instance;
+            }
+        }
+
+        private Dictionary<Profile, ScoreInfo> _scores = new();
+    }
+}
