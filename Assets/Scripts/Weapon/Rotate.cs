35bc26b49581edc307613deec1b3be9f4045feaf
Xwilarg <xwilarg@protonmail.com>
2022-04-24 18:33:00 +0200

    Add objects

---
+++ Assets/Scripts/Weapon/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace MiniJameGam9.Weapon
+{
+    internal class Rotate : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.Rotate(new Vector3(0f, Time.deltaTime, 0f));
+        }
+    }
+}
